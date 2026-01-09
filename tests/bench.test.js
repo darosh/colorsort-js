ce5eba548a2a58a76d032d4a3e184d48a7747425
Jan Forst <ensonador@gmail.com>
2026-01-09 12:16:32 +0100

    bench total

--- tests/bench.test.js
+++ tests/bench.test.js
@@ -13,18 +13,22 @@ test('bench', async () => {
       const start = performance.now()
       fn(palette)
       const elapsed = performance.now() - start
-      
+
       const id = mid.replace(/:\[.+]/, '')
-      
+
       data[id] = data[id] || { count: 0, elapsed: 0}
       data[id].count++
       data[id].elapsed += elapsed
-      
+
       // console.log(speed, name, elapsed)
     })
   })
   
   const t = Object.entries(data).map(([key, value]) => [key, Math.round(Math.log10(1 + 1000 * value.elapsed / value.count))]).sort((a, b) => a[1] - b[1])
+  const sum = t.reduce((acc, [, val]) => acc + val, 0)
+  
+  t.push(['_TOTAL', sum])
+
   const b = Object.fromEntries(t)
   
   console.log(b)
