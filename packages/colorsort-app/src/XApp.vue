1018479e47491914c0d53130b6912519f6f5c9be
Jan Forst <ensonador@gmail.com>
2026-01-19 10:38:51 +0100

    "responsive" layout

--- packages/colorsort-app/src/XApp.vue
+++ packages/colorsort-app/src/XApp.vue
@@ -122,10 +122,10 @@ a.link-grey {
 </style>
 
 <template>
-  <v-app theme="dark" class="">
+  <v-app theme="dark">
     <!--    <v-navigation-drawer v-model="showNav" width="423"></v-navigation-drawer>-->
 
-    <v-app-bar flat id="app-bar" v-model="appBar" :scroll-behavior="hideBar ? 'hide' : null" :scroll-threshold="72" color="transparent">
+    <v-app-bar overflow-x: auto; flat id="app-bar" v-model="appBar" :scroll-behavior="hideBar ? 'hide' : null" :scroll-threshold="72" color="transparent">
       <!--      <v-app-bar-nav-icon @click="showNav = !showNav" />-->
       <v-app-bar-title class="flex-0-0 mr-4" style="width: 194px;">Color Sorting R&D</v-app-bar-title>
       <v-toolbar-items class="mr-4">
@@ -133,7 +133,7 @@ a.link-grey {
         <v-btn href="./#/stats">Statistics</v-btn>
       </v-toolbar-items>
       <v-spacer />
-      <div v-show="routeLoaded">
+      <div v-show="routeLoaded && isWide">
         <span class="ml-4 text-grey-lighten-1"
           >{{ number(methodsCount) }}
           {{
@@ -165,7 +165,7 @@ a.link-grey {
           of {{ number(totalGroups) }} {{ totalGroups === 1 ? 'result' : 'results' }}</span
         >
       </div>
-      <template v-if="!showStats">
+      <template v-if="!showStats && isWide">
         <v-text-field @focus="onFocusInA" @focusout="onFocusOutA" clearable prepend-icon="mdi-magnify" autocomplete="off" v-model.lazy="filterPalette" hide-details class="align-self-center mr-4 mt-1" placeholder="Palette" density="compact" variant="solo-filled" max-width="220">
           <template v-slot:append-inner>
             <v-icon id="help-p" style="cursor: help;" @mouseenter="(e) => { cursor = cxy(e); menu = true; }" @mouseleave="() => { menu = false }"> mdi-help-circle </v-icon>
@@ -180,7 +180,7 @@ a.link-grey {
       </template>
 
       <template v-slot:extension>
-        <div v-if="!showStats && routeLoaded" class="d-flex" style="width: 100%;">
+        <div v-if="!showStats && routeLoaded && isWide" class="d-flex" style="width: 100%;">
           <div style="width: 230px;" class="flex-grow-0 px-8"></div>
           <div class="d-flex ext flex-grow-1" style="flex-direction: row; height: 48px; padding-top: 12px;">
             <div style="width: 210px;" class="flex-grow-0"></div>
@@ -194,7 +194,7 @@ a.link-grey {
             <div class="flex-grow-1"></div>
           </div>
         </div>
-        <div v-else-if="showStats && routeLoaded" class="d-flex ext" style="width: 100%; flex-direction: row; height: 48px; align-items: center">
+        <div v-else-if="showStats && routeLoaded && isWide" class="d-flex ext" style="width: 100%; flex-direction: row; height: 48px; align-items: center">
           <div class="mr-8 ml-4">
             <a class="link" :href="`./#/?m=${encodeURIComponent('#')}${targetCoverage}`">Target method coverage</a>: {{ targetCoverage }} {{ targetCoverage === 1 ? 'palette' : 'palettes' }} with
             {{ algorithmStatsFilteredIncl?.length }}
@@ -231,110 +231,115 @@ a.link-grey {
                   (methods.length === 1 || isExpanded(__key)) ? methods.map(m => m.method.mid).join('\n') : `${methods[0].method.mid} ...+${methods.length - 1}`
                 }}
               </div>
-              <div class="text-pre text-right flex-grow-0" style="width: 90px; cursor: pointer;" @click.stop="expandIndex(__key)">
-                {{
+              <template v-if="isWide">
+                <div class="text-pre text-right flex-grow-0" style="width: 90px; cursor: pointer;" @click.stop="expandIndex(__key)">
+                  {{
                   (methods.length === 1 || isExpanded(__key)) ? methods.map(({ time }) => time !== null ? `${time.toFixed(0)} ms` : '...').join('\n') : `... ${methods[0].time.toFixed(0)}ms`
-                }}
-              </div>
+                  }}
+                </div>
 
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.totalDistance)}">
-                <template v-if="quality?.totalDistance === 0">*</template>
-                <template v-if="metrics">{{ metrics.totalDistance.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.meanDistance)}">
-                <template v-if="quality?.meanDistance === 0">*</template>
-                <template v-if="metrics">{{ metrics.meanDistance.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.devDistance)}">
-                <template v-if="quality?.devDistance === 0">*</template>
-                <template v-if="metrics">{{ metrics.devDistance.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.totalDistance)}">
+                  <template v-if="quality?.totalDistance === 0">*</template>
+                  <template v-if="metrics">{{ metrics.totalDistance.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
 
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.totalCurveDistance)}">
-                <template v-if="quality?.totalCurveDistance === 0">*</template>
-                <template v-if="metrics">{{ metrics.totalCurveDistance.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.meanCurveDistance)}">
-                <template v-if="quality?.meanCurveDistance === 0">*</template>
-                <template v-if="metrics">{{ metrics.meanCurveDistance.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.devCurveDistance)}">
-                <template v-if="quality?.devCurveDistance === 0">*</template>
-                <template v-if="metrics">{{ metrics.devCurveDistance.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.meanDistance)}">
+                  <template v-if="quality?.meanDistance === 0">*</template>
+                  <template v-if="metrics">{{ metrics.meanDistance.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.devDistance)}">
+                  <template v-if="quality?.devDistance === 0">*</template>
+                  <template v-if="metrics">{{ metrics.devDistance.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
 
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.curveRatio)}">
-                <template v-if="quality?.curveRatio === 0">*</template>
-                <template v-if="metrics">{{ (100 * metrics.curveRatio).toFixed(1) }}%</template>
-                <template v-else>...</template>
-              </div>
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.avgAngleChange)}">
-                <template v-if="quality?.avgAngleChange === 0">*</template>
-                <template v-if="metrics">{{ metrics.avgAngleChange.toFixed() }}&deg;</template>
-                <template v-else>...</template>
-              </div>
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.maxAngleChange)}">
-                <template v-if="quality?.maxAngleChange === 0">*</template>
-                <template v-if="metrics">{{ metrics.maxAngleChange.toFixed() }}&deg;</template>
-                <template v-else>...</template>
-              </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.totalCurveDistance)}">
+                  <template v-if="quality?.totalCurveDistance === 0">*</template>
+                  <template v-if="metrics">{{ metrics.totalCurveDistance.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.meanCurveDistance)}">
+                  <template v-if="quality?.meanCurveDistance === 0">*</template>
+                  <template v-if="metrics">{{ metrics.meanCurveDistance.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.devCurveDistance)}">
+                  <template v-if="quality?.devCurveDistance === 0">*</template>
+                  <template v-if="metrics">{{ metrics.devCurveDistance.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
 
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.perceptualUniformity)}">
-                <template v-if="quality?.perceptualUniformity === 0">*</template>
-                <template v-if="metrics">{{ metrics.perceptualUniformity.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.curveRatio)}">
+                  <template v-if="quality?.curveRatio === 0">*</template>
+                  <template v-if="metrics">{{ (100 * metrics.curveRatio).toFixed(1) }}%</template>
+                  <template v-else>...</template>
+                </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.avgAngleChange)}">
+                  <template v-if="quality?.avgAngleChange === 0">*</template>
+                  <template v-if="metrics">{{ metrics.avgAngleChange.toFixed() }}&deg;</template>
+                  <template v-else>...</template>
+                </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.maxAngleChange)}">
+                  <template v-if="quality?.maxAngleChange === 0">*</template>
+                  <template v-if="metrics">{{ metrics.maxAngleChange.toFixed() }}&deg;</template>
+                  <template v-else>...</template>
+                </div>
 
-              <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.harmonicScore)}">
-                <template v-if="quality?.harmonicScore === 0">*</template>
-                <template v-if="metrics">{{ metrics.harmonicScore.toFixed(2) }}</template>
-                <template v-else>...</template>
-              </div>
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.perceptualUniformity)}">
+                  <template v-if="quality?.perceptualUniformity === 0">*</template>
+                  <template v-if="metrics">{{ metrics.perceptualUniformity.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
 
-              <div style="width: 100px; line-height: 14px; font-size: 12px;" class="text-center text-no-wrap flex-grow-0 py-2">
-                <template v-if="metrics">
-                  <span :style="{color: scale(quality?.lchAvgChange.L)}">{{
+                <div style="width: 60px" class="text-right px-1 flex-grow-0" :style="{color: scale(quality?.harmonicScore)}">
+                  <template v-if="quality?.harmonicScore === 0">*</template>
+                  <template v-if="metrics">{{ metrics.harmonicScore.toFixed(2) }}</template>
+                  <template v-else>...</template>
+                </div>
+
+                <div style="width: 100px; line-height: 14px; font-size: 12px;" class="text-center text-no-wrap flex-grow-0 py-2">
+                  <template v-if="metrics">
+                    <span :style="{color: scale(quality?.lchAvgChange.L)}">{{
                       metrics.lchAvgChange.L.toFixed(0)
-                  }}</span
-                  >,
-                  <span :style="{color: scale(quality?.lchAvgChange.C)}">{{
+                    }}</span
+                    >,
+                    <span :style="{color: scale(quality?.lchAvgChange.C)}">{{
                       metrics.lchAvgChange.C.toFixed(0)
-                  }}</span
-                  >,
-                  <span :style="{color: scale(quality?.lchAvgChange.H)}">{{
+                    }}</span
+                    >,
+                    <span :style="{color: scale(quality?.lchAvgChange.H)}">{{
                       metrics.lchAvgChange.H.toFixed(0)
-                  }}</span
-                  ><br />
-                  <span :style="{color: scale(quality?.lchMaxChange.L)}">{{ metrics.lchMaxChange.L.toFixed(0) }}</span
-                  >,
-                  <span :style="{color: scale(quality?.lchMaxChange.C)}">{{
+                    }}</span
+                    ><br />
+                    <span :style="{color: scale(quality?.lchMaxChange.L)}">{{ metrics.lchMaxChange.L.toFixed(0) }}</span
+                    >,
+                    <span :style="{color: scale(quality?.lchMaxChange.C)}">{{
                       metrics.lchMaxChange.C.toFixed(0)
-                  }}</span
-                  >,
-                  <span :style="{color: scale(quality?.lchMaxChange.H)}">{{
+                    }}</span
+                    >,
+                    <span :style="{color: scale(quality?.lchMaxChange.H)}">{{
                       metrics.lchMaxChange.H.toFixed(0)
-                  }}</span
-                  ><br />
-                  <span :style="{color: scale(quality?.lchDeviation.L)}">{{ metrics.lchDeviation.L.toFixed(0) }}</span
-                  >,
-                  <span :style="{color: scale(quality?.lchDeviation.C)}">{{
+                    }}</span
+                    ><br />
+                    <span :style="{color: scale(quality?.lchDeviation.L)}">{{ metrics.lchDeviation.L.toFixed(0) }}</span
+                    >,
+                    <span :style="{color: scale(quality?.lchDeviation.C)}">{{
                       metrics.lchDeviation.C.toFixed(0)
-                  }}</span
-                  >,
-                  <span :style="{color: scale(quality?.lchDeviation.H)}">{{
+                    }}</span
+                    >,
+                    <span :style="{color: scale(quality?.lchDeviation.H)}">{{
                       metrics.lchDeviation.H.toFixed(0)
-                  }}</span>
-                </template>
-              </div>
-              <div style="width: 32px" class="text-right px-1 flex-grow-0" :style="{color: scale(bestDistanceQuality)}">
-                {{ bestDistance !== null ? (!bestDistance ? 0 : bestDistance.toFixed(2)) : '...' }}
-              </div>
+                    }}</span>
+                  </template>
+                </div>
+
+                <div style="width: 32px" class="text-right px-1 flex-grow-0" :style="{color: scale(bestDistanceQuality)}">
+                  {{ bestDistance !== null ? (!bestDistance ? 0 : bestDistance.toFixed(2)) : '...' }}
+                </div>
+              </template>
+
               <div style="width: 64px" class="text-center pr-0 pl-5 flex-grow-0">
                 <v-checkbox-btn style="margin-right: -6px; margin-left: -4px;" :model-value="methods.some(m => m.best)" @click.stop="e => bestChange(e, key, methods[0].index, methods.some(m => m.best))" />
               </div>
@@ -1057,6 +1062,9 @@ export default {
     },
     focusPending () {
       return this.focusPendingA || this.focusPendingB
+    },
+    isWide () {
+      return this.$vuetify.display.width > 1600
     }
   },
   watch: {
