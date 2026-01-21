06b8b93017f1fd05b2b6bf63eefd00b219966d8e
Jan Forst <ensonador@gmail.com>
2026-01-21 15:18:18 +0100

    lib export

--- packages/colorsort/src/index.ts
+++ packages/colorsort/src/index.ts
@@ -6,6 +6,10 @@ export type { PaletteType } from './type-detect.ts'
 
 export { metricsEx, getMetricsExRange, metricsExQuality, metricsExQualitySum } from './metrics-extended.ts'
 export type { MetricsEx } from './metrics-extended.ts'
-export { flatRgb, oklab, oklch, gl, lch, lab, normalizeLab } from './color.ts'
+export { flatRgb, oklab, oklch, gl, lch, lab, normalizeLab, oklch2hex, oklch2oklab } from './color.ts'
 
 export { randomizer } from './randomizer.ts'
+export { metricsFft, cosineSimilarity, extractSpectralFeaturesOklab, compareSpectralFeatures } from './metrics-fft.ts'
+export type { Fingerprint, SpectralFeatures } from './metrics-fft.ts'
+
+export { applySpectralProcessing } from './spectral-edit.ts'
